a03f057abd8e07c8376e31413a399ac194a71c02
GruffCassquatch <cassanda.agius@gmail.com>
2022-11-05 08:03:14 +1100

    Add project files.

---
+++ src/Implementation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace NoKeepersPassTransition
+{
+    public class Implementation : MelonMod
+    {
+        public const string keepersPassTransition = "Design/Transitions/CanyonRoad";
+
+
+        public override void OnApplicationStart()
+        {
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+        }
+    }
+}
---
+++ src/Patches.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace NoKeepersPassTransition
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(GameManager), "Awake")]
+        internal class DisableKeepersPassTransition
+        {
+            private static void Postfix()
+            {
+                if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != "RuralRegion") return;
+                if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Story || ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.StoryFresh || ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.StoryHardened) return;
+
+                GameObject keepersPassTransition = GameObject.Find(Implementation.keepersPassTransition);
+
+                if (keepersPassTransition is null)
+                {
+                    MelonLoader.MelonLogger.Error("Could not find keepersPassTransition");
+                    return;
+                }
+                else
+                {
+                    keepersPassTransition.SetActive(false);
+                }
+            }
+        }
+    }
+}
